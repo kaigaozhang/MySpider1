d59d0ec3f9b55921d14fdfdf1b4424a2e798e8fe
kkllor <Adlof@163.com>
2013-12-27 17:05:06 +0800

    as

--- src/com/spider/main/Launcher.java
+++ src/com/spider/main/Launcher.java
@@ -7,7 +7,6 @@ import com.spider.entity.Page;
 public class Launcher {
 	public static void main(String[] args) throws Exception {
 		Page page = new HttpSpider().getHtmlContent(new URL("http://www.sina.com.cn/"), "utf-8");
-		// System.out.println(page.getContent()) ; 
 	System.exit(0);
 	}
 	 
